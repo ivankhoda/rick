3d9046ff3e8a746295626032daf7deda148e2b99
Ivan Khoda <ivankhoda@gmail.com>
2021-11-01 23:13:20 +0300

    Commit before add R&M api package

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,7 @@ const App: React.FC = () => {
   const getData = async (link: string) => {
     fetch(`${link}`, {
       method: "GET",
+
       headers: {
         "Content-Type": "application/json",
       },
